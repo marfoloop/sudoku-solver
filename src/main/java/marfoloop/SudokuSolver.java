7983e6983feac01c46d1e6c0f88785cd45f5b466
marfo zaciatky <mkocun@gmail.com>
2016-04-15 23:16:38 +0200

    init

---
+++ src/main/java/marfoloop/SudokuSolver.java
@@ -0,0 +1,12 @@
+package marfoloop;
+
+/**
+ * Created by marflen on 4/11/2016.
+ */
+
+public class SudokuSolver {
+   static class
+    public static void main(String[] args) {
+        System.out.println("fero");
+    }
+}
