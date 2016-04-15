0e119a9689869a1f04a61ad5be8ae6e238f98d8e
marfo zaciatky <mkocun@gmail.com>
2016-04-15 23:33:12 +0200

    marfo

--- src/main/java/marfoloop/SudokuSolver.java
+++ src/main/java/marfoloop/SudokuSolver.java
@@ -7,6 +7,6 @@ package marfoloop;
 public class SudokuSolver {
    static class
     public static void main(String[] args) {
-        System.out.println("fero");
+        System.out.println("marfo");
     }
 }
